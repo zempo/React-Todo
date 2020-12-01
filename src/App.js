1501129c3b9225ba85c476dc6e8efce5d2ec0151
zempo <zelenkosolomon@gmail.com>
2020-11-30 17:10:57 -0800

    add initial todos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+const initialTodos = [
+  {
+    task: "Organize Garage",
+    id: uuidv4(),
+    completed: false,
+  },
+  {
+    task: "Run with the bulls",
+    id: uuidv4(),
+    completed: false,
+  },
+  {
+    task: "Existential Panic",
+    id: uuidv4(),
+    completed: false,
+  },
+  {
+    task: "Bake Cookies",
+    id: uuidv4(),
+    completed: false,
+  },
+]
 
 class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: initialTodos
+    }
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
       <div>
-        <h1>To Do or Not To Do</h1>
+        <h2>To Do or Not To Do</h2>
       </div>
     );
   }
---
+++ src/components/TodoForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TodoForm extends Component {
+    render() {
+        return (
+            <div>
+                <h3>blah</h3>
+            </div>
+        )
+    }
+}
